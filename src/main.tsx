02daa4ba1ca25c8f41e2174e52fe76d28653432a
Steven Kim <stevenn.kkim@gmail.com>
2024-06-27 17:33:01 -0700

    Added routes using TanStack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import ProfileCard from "./challenge-components/ProfileCard/ProfileCard.tsx";
+import "./index.css";
+import {
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootRoute = createRootRoute({
+  component: App,
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <div>home</div>,
+});
+
+const profileCardRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/profile-card",
+  component: ProfileCard,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, profileCardRoute]);
+
+const router = createRouter({ routeTree });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
