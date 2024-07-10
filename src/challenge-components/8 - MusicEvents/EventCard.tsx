344ac82707d9755925199d0aa81f3e2cbaa6c0e7
Steven Kim <stevenn.kkim@gmail.com>
2024-07-10 13:55:43 -0700

    Event card component - displays information for events

--- src/challenge-components/8 - MusicEvents/EventCard.tsx
+++ src/challenge-components/8 - MusicEvents/EventCard.tsx
@@ -1,5 +1,29 @@
-const EventCard = () => {
-  return <div></div>;
+import type { TEvent } from "./eventsData";
+import EventDate from "./components/EventDate";
+import BuyTicketBtn from "./components/BuyTicketBtn";
+
+interface Props {
+  event: TEvent;
+}
+
+const EventCard = ({ event }: Props) => {
+  const { name, artist, venue, venueAddress, date, availableTickets } = event;
+
+  return (
+    <div className="flex flex-row items-center gap-4 rounded-lg bg-white p-4">
+      <EventDate dateString={date} />
+      <p className="flex flex-col">
+        <span className="text-xl font-semibold">
+          {name} - {artist}
+        </span>
+        <span>{venue}</span>
+        <span className="text-sm text-muted">{venueAddress}</span>
+      </p>
+      <div className="ml-auto flex min-w-[120px] items-center justify-center text-center">
+        {availableTickets === 0 ? <>Sold out!</> : <BuyTicketBtn />}
+      </div>
+    </div>
+  );
 };
 
 export default EventCard;
