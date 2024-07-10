220274dc74bb8a159e4ed446d9af3d9c4aa71b20
Steven Kim <stevenn.kkim@gmail.com>
2024-07-10 13:56:27 -0700

    Event list - displays EventCard for each event from the list

--- src/challenge-components/8 - MusicEvents/EventsList.tsx
+++ src/challenge-components/8 - MusicEvents/EventsList.tsx
@@ -1,5 +1,18 @@
-const EventsList = () => {
-  return <div>events list</div>;
+import EventCard from "./EventCard";
+import type { TEvent } from "./eventsData";
+
+interface Props {
+  list: TEvent[];
+}
+
+const EventsList = ({ list }: Props) => {
+  return (
+    <div className="mt-4 flex flex-col gap-2">
+      {list.map((event) => {
+        return <EventCard key={event.name} event={event} />;
+      })}
+    </div>
+  );
 };
 
 export default EventsList;
--- src/challenge-components/8 - MusicEvents/MusicEventsPage.tsx
+++ src/challenge-components/8 - MusicEvents/MusicEventsPage.tsx
@@ -1,11 +1,12 @@
 import EventsHeader from "./EventsHeader";
 import EventsList from "./EventsList";
+import { EventsData } from "./eventsData";
 
 const MusicEventsPage = () => {
   return (
     <section className="h-page flex w-full flex-col bg-blue-200 p-[10%]">
       <EventsHeader location="Seattle, Washington" />
-      <EventsList />
+      <EventsList list={EventsData} />
     </section>
   );
 };
