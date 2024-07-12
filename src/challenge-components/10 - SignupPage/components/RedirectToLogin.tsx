8dfff0fa201a6f10a0dbbff7e7397e4904b8129f
Steven Kim <stevenn.kkim@gmail.com>
2024-07-12 14:26:50 -0700

    Redirect text with anchor tag component

---
+++ src/challenge-components/10 - SignupPage/components/RedirectToLogin.tsx
@@ -0,0 +1,10 @@
+const RedirectToLogin = () => {
+  return (
+    <div className="w-full text-center text-sm">
+      Already have an account?{" "}
+      <a className="text-blue-400 hover:text-blue-600">Log In</a>
+    </div>
+  );
+};
+
+export default RedirectToLogin;
