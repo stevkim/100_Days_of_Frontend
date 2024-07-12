cb3e9eb22e86877aa5dc602663a6e861a93dcf6b
Steven Kim <stevenn.kkim@gmail.com>
2024-07-12 14:26:26 -0700

    Signup page header

---
+++ src/challenge-components/10 - SignupPage/components/SignupHeader.tsx
@@ -0,0 +1,12 @@
+const SignupHeader = () => {
+  return (
+    <div>
+      <h2 className="my-4 text-center text-2xl font-semibold">Sign Up</h2>
+      <p className="text-sm">
+        Join us now! Sign up to kick-start your journey.
+      </p>
+    </div>
+  );
+};
+
+export default SignupHeader;
