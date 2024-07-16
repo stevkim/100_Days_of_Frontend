9bbc95c9c0962ef5bdab7ef8b5ace64ab6579ff9
Steven Kim <stevenn.kkim@gmail.com>
2024-07-16 13:02:37 -0700

    Concept done - for the drag and drop feature of TaskBoard

--- src/challenge-components/13 - TaskBoard/TaskBoard.tsx
+++ src/challenge-components/13 - TaskBoard/TaskBoard.tsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const TaskBoard = () => {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const draggedRef = useRef<HTMLDivElement | null>(null);
+
+  const one = tasks.filter((task) => task[0] === "1");
+  const two = tasks.filter((task) => task[0] === "2");
+  const three = tasks.filter((task) => task[0] === "3");
+
+  const handleAdd = (board: string, value: string = "test") => {
+    setTasks([...tasks, `${board}. ${value}`]);
+  };
+
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    draggedRef.current = e.currentTarget;
+  };
+
+  const handleDragEnd = () => {
+    draggedRef.current = null;
+  };
+
   const dragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
@@ -8,6 +27,18 @@ const TaskBoard = () => {
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     console.log(e.currentTarget.id);
+
+    const newList = tasks.map((task) => {
+      if (task === draggedRef.current?.innerText) {
+        return (
+          e.currentTarget.id.substring(e.currentTarget.id.length - 1) +
+          task.substring(1)
+        );
+      }
+      return task;
+    });
+
+    setTasks(newList);
   };
 
   return (
@@ -18,10 +49,24 @@ const TaskBoard = () => {
         onDragOver={dragOver}
         onDrop={handleDrop}
       >
-        <h4>Board 1</h4>
-        <div className="absolute" draggable="true">
-          task
+        <div className="flex justify-evenly">
+          <h4>Board 1</h4>
+          <button onClick={() => handleAdd("1", `testing ${Math.random()}`)}>
+            +
+          </button>
         </div>
+        {one.map((text, index) => {
+          return (
+            <div
+              draggable="true"
+              key={"board-one" + text + index}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            >
+              {text}
+            </div>
+          );
+        })}
       </div>
       <div
         id="board-2"
@@ -29,7 +74,24 @@ const TaskBoard = () => {
         onDragOver={dragOver}
         onDrop={handleDrop}
       >
-        <h4>Board 2</h4>
+        <div className="flex justify-evenly">
+          <h4>Board 2</h4>
+          <button onClick={() => handleAdd("2", `testing ${Math.random()}`)}>
+            +
+          </button>
+        </div>
+        {two.map((text, index) => {
+          return (
+            <div
+              draggable="true"
+              key={"board-two" + text + index}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            >
+              {text}
+            </div>
+          );
+        })}
       </div>
       <div
         id="board-3"
@@ -37,7 +99,24 @@ const TaskBoard = () => {
         onDragOver={dragOver}
         onDrop={handleDrop}
       >
-        <h4>Board 3</h4>
+        <div className="flex justify-evenly">
+          <h4>Board 3</h4>
+          <button onClick={() => handleAdd("3", `testing ${Math.random()}`)}>
+            +
+          </button>
+        </div>
+        {three.map((text, index) => {
+          return (
+            <div
+              draggable="true"
+              key={"board-three" + text + index}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            >
+              {text}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
