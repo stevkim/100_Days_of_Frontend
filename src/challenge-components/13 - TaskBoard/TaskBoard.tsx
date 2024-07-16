b660fa84386d725faf01554944d51e33de474015
Steven Kim <stevenn.kkim@gmail.com>
2024-07-16 12:43:04 -0700

    Figuring our Drag and drop api

--- src/challenge-components/13 - TaskBoard/TaskBoard.tsx
+++ src/challenge-components/13 - TaskBoard/TaskBoard.tsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 const TaskBoard = () => {
-  return <div>TaskBoard</div>;
+  const dragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log(e.currentTarget.id);
+  };
+
+  return (
+    <section className="relative flex h-page w-full flex-row items-center justify-center gap-10">
+      <div
+        id="board-1"
+        className="h-[300px] w-[200px] border border-blue-200"
+        onDragOver={dragOver}
+        onDrop={handleDrop}
+      >
+        <h4>Board 1</h4>
+        <div className="absolute" draggable="true">
+          task
+        </div>
+      </div>
+      <div
+        id="board-2"
+        className="h-[300px] w-[200px] border border-blue-200"
+        onDragOver={dragOver}
+        onDrop={handleDrop}
+      >
+        <h4>Board 2</h4>
+      </div>
+      <div
+        id="board-3"
+        className="h-[300px] w-[200px] border border-blue-200"
+        onDragOver={dragOver}
+        onDrop={handleDrop}
+      >
+        <h4>Board 3</h4>
+      </div>
+    </section>
+  );
 };
 
 export default TaskBoard;
