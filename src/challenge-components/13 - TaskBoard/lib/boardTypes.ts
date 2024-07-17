0218cd55b576a4fef24a46f39dbdc33781594875
Steven Kim <stevenn.kkim@gmail.com>
2024-07-17 13:08:11 -0700

    Refactor exports into a single index file
    Added board types -> to display the correct board name

---
+++ src/challenge-components/13 - TaskBoard/lib/boardTypes.ts
@@ -0,0 +1,10 @@
+type TBoard = {
+  [prop: string]: string;
+};
+
+export const boardTypes: TBoard = {
+  todo: "To do",
+  inprogress: "In progress",
+  underreview: "Under review",
+  done: "Done",
+};
---
+++ src/challenge-components/13 - TaskBoard/lib/index.ts
@@ -0,0 +1,6 @@
+import { boardTypes } from "./boardTypes";
+import { getRandomId } from "./getRandomId";
+import { formatDate } from "./formatDate";
+import { validateForm } from "./validateForm";
+
+export { boardTypes, getRandomId, formatDate, validateForm };
