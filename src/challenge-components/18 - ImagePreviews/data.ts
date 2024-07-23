2b441fc2f54264c83a07ae36ff578734e75b4ff0
Steven Kim <stevenn.kkim@gmail.com>
2024-07-23 13:51:14 -0700

    Added mock data for image preview

---
+++ src/challenge-components/18 - ImagePreviews/data.ts
@@ -0,0 +1,65 @@
+import type { TImage } from "./types/imageType";
+
+import image1 from "@/assets/6 - ImageCarousel/nature1.avif";
+import image2 from "@/assets/6 - ImageCarousel/nature2.avif";
+import image3 from "@/assets/6 - ImageCarousel/nature3.avif";
+import image4 from "@/assets/6 - ImageCarousel/nature4.avif";
+import image5 from "@/assets/6 - ImageCarousel/nature5.avif";
+import image6 from "@/assets/6 - ImageCarousel/nature6.avif";
+import image7 from "@/assets/6 - ImageCarousel/nature7.avif";
+import image8 from "@/assets/6 - ImageCarousel/nature8.avif";
+import image9 from "@/assets/6 - ImageCarousel/nature9.avif";
+import image10 from "@/assets/6 - ImageCarousel/nature10.avif";
+
+export const IMAGES: TImage[] = [
+  {
+    id: 0,
+    image: image1,
+    title: "Title 1",
+  },
+  {
+    id: 1,
+    image: image2,
+    title: "Title 2",
+  },
+  {
+    id: 2,
+    image: image3,
+    title: "Title 3",
+  },
+  {
+    id: 3,
+    image: image4,
+    title: "Title 4",
+  },
+  {
+    id: 4,
+    image: image5,
+    title: "Title 5",
+  },
+  {
+    id: 5,
+    image: image6,
+    title: "Title 6",
+  },
+  {
+    id: 6,
+    image: image7,
+    title: "Title 7",
+  },
+  {
+    id: 7,
+    image: image8,
+    title: "Title 8",
+  },
+  {
+    id: 8,
+    image: image9,
+    title: "Title 9",
+  },
+  {
+    id: 9,
+    image: image10,
+    title: "Title 10",
+  },
+];
